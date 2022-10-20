881aa09ef84ca9c018141a33c81f255fb2f38da9
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-20 02:53:33 -0400

    v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useEffect} from "react";
+import axios from "axios"
 function App() {
+
+  const x="";
+  React.useEffect(() => {
+   
+    axios.get(`/api/test`).then(res => {
+          console.log(res);
+          x=res;
+    })
+        
+      }
+   );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+hello{x}
+     </h1>
     </div>
   );
 }
